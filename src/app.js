0b2447147cb506d05d8f98b082c1378f5cb96468
SMAIAH <smaiah.sarah@gmail.com>
2020-11-11 16:54:07 +0100

    add src et change json files

---
+++ src/app.js
@@ -0,0 +1,12 @@
+'use strict'
+
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+
+app.use(morgan('dev'))
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+module.exports = app
